65a8389add2ddfdf8d59875835ae7fcc8b76318d
yuepang <yuepang@digitalgd.com.cn>
2024-03-13 08:40:56 +0800

    更新

--- deleteUnusedImg.py
+++ deleteUnusedImg.py
@@ -1,13 +1,16 @@
 import re
 import os
 
-def traverse_dir(path):
+deletedFiles = []
+deletedNum = 0
+
+def traverse_dir(path, mode):
     dirs = os.listdir(path)
     for item in dirs:
         itemPath = os.path.join(path, item)
         # 如果遍历到的是文件夹，则递归进入文件夹去判断 需要删除其中的assets文件夹下的多余图片
         if os.path.isdir(itemPath):
-            traverse_dir(itemPath)
+            traverse_dir(itemPath, mode)
 
         if os.path.isfile(itemPath):
             # 文件名
@@ -15,10 +18,11 @@ def traverse_dir(path):
             if f.endswith('.md'):
                 print('自动识别的md文件：', f)
                 mdPath = os.path.join(path, f)
+                fileName = f[:-3]
                 # 判断是否有assets文件夹，没有直接退出
-                assetPath = os.path.join(path, 'assets')
+                assetPath = os.path.join(path, 'assets') if mode == 'a' else os.path.join(path, f'{fileName}.assets')
                 if not os.path.exists(assetPath):
-                    print('该文件夹下无 assets 文件夹，跳过')
+                    print('该文件夹下无 assets 文件夹，跳过\n')
                     continue
                 print('md文件路径：', mdPath)
                 print('asset文件夹路径：', assetPath)
@@ -27,17 +31,19 @@ def traverse_dir(path):
 
 
 
-def traverse_delete(path):
-    currentPath = os.getcwd()
-    traverse_dir(currentPath)
+def traverse_delete(path, mode):
+    currentPath = os.getcwd() if path == '' else path
+    traverse_dir(currentPath, mode)
 
 # 由于obsidian在粘贴图片时，会自动将md文件中的图片路径中的空格转码为%20，但是不会转换assets文件夹中的图片路径，故在这里做一下替换
 def decodeSpace(text):
     return text.replace('%20', ' ')
 
+# 原来这个函数，只会读取一个文件，不够完善。实际上应该读取多个文件
 def delete_img(mdPath, assetPath='assets'):
     with open(mdPath, 'r', encoding='utf-8') as file:
         content = file.read()
+        print(content)
         pattern = r'!\[.*\]\(assets/(.*?)\)'
         matches = re.findall(pattern, content)
         dealedMatches = []
@@ -53,8 +59,18 @@ def delete_img(mdPath, assetPath='assets'):
                 print('删除多余文件 ', f)
                 removedFilePath = os.path.join(assetPath, f)
                 os.remove(removedFilePath)
+                # 已删除的文件加入统计列表
+                deletedFiles.append(removedFilePath)
+                deletedNum = deletedNum + 1
         print('删除完成\n')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    traverse_delete('./')
+    print('\n使用前请将本代码放置于需要去除md文件的重复图片的文件夹下。\n')
+    mode = ''
+    while mode != 'a' and mode != 'b':
+        mode = input('输入a或b选择不同的图片文件夹识别模式：(a)assets （b) filename.assets\n')
+    print('选择模式', mode)
+    traverse_delete('', mode)
+    print('共删除以下文件：', deletedFiles)
+    print('共删除文件数：', deletedNum)
