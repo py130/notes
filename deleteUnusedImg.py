b33e1e1639d1c3c53d488ec51c48634ea10afa06
yuepang <yuepang@digitalgd.com.cn>
2024-03-15 15:22:20 +0800

    更新

--- deleteUnusedImg.py
+++ deleteUnusedImg.py
@@ -5,29 +5,54 @@ deletedFiles = []
 deletedNum = 0
 
 def traverse_dir(path, mode):
+    print(f'\n当前遍历文件夹： {path}')
     dirs = os.listdir(path)
-    for item in dirs:
-        itemPath = os.path.join(path, item)
-        # 如果遍历到的是文件夹，则递归进入文件夹去判断 需要删除其中的assets文件夹下的多余图片
-        if os.path.isdir(itemPath):
-            traverse_dir(itemPath, mode)
 
-        if os.path.isfile(itemPath):
-            # 文件名
-            f = item
-            if f.endswith('.md'):
-                print('自动识别的md文件：', f)
-                mdPath = os.path.join(path, f)
-                fileName = f[:-3]
-                # 判断是否有assets文件夹，没有直接退出
-                assetPath = os.path.join(path, 'assets') if mode == 'a' else os.path.join(path, f'{fileName}.assets')
-                if not os.path.exists(assetPath):
-                    print('该文件夹下无 assets 文件夹，跳过\n')
-                    continue
-                print('md文件路径：', mdPath)
-                print('asset文件夹路径：', assetPath)
+    if mode == 'b':
+        for item in dirs:
+            itemPath = os.path.join(path, item)
+            # 如果遍历到的是文件夹，则递归进入文件夹去判断 需要删除其中的assets文件夹下的多余图片
+            if os.path.isdir(itemPath):
+                traverse_dir(itemPath, mode)
+            if os.path.isfile(itemPath):
+                # 文件名
+                f = item
+                if f.endswith('.md'):
+                    print('自动识别的md文件：', f)
+                    mdPath = os.path.join(path, f)
+                    fileName = f[:-3]
+                    # 判断是否有assets文件夹，没有直接退出
+                    assetPath = os.path.join(path, f'{fileName}.assets')
+                    if not os.path.exists(assetPath):
+                        print(f'该文件夹下无对应文件 "{fileName}" 的 {fileName}.assets 文件夹，跳过该文件\n')
+                        continue
+                    print('md文件路径：', mdPath)
+                    print('asset文件夹路径：', assetPath)
 
-                delete_img(mdPath, assetPath)
+                    delete_img([mdPath], assetPath)
+    elif mode == 'a':
+        # 判断是否有assets文件夹，没有直接退出
+        assetPath = os.path.join(path, 'assets')
+        if not os.path.exists(assetPath):
+            print('该文件夹下无 assets 文件夹，跳过')
+            return
+        fileNameList = []
+        for item in dirs:
+            itemPath = os.path.join(path, item)
+            # 如果遍历到的是文件夹，则递归进入文件夹去判断 需要删除其中的assets文件夹下的多余图片
+            if os.path.isdir(itemPath):
+                traverse_dir(itemPath, mode)
+            if os.path.isfile(itemPath):
+                # 文件名
+                f = item
+                if f.endswith('.md'):
+                    mdPath = os.path.join(path, f)
+                    fileNameList.append(mdPath)
+        print(f'\n当前遍历文件夹： {path}')
+        print('md文件路径：', fileNameList)
+        print('asset文件夹路径：', assetPath)
+
+        delete_img(fileNameList, assetPath)
 
 
 
@@ -39,30 +64,34 @@ def traverse_delete(path, mode):
 def decodeSpace(text):
     return text.replace('%20', ' ')
 
-# 原来这个函数，只会读取一个文件，不够完善。实际上应该读取多个文件
-def delete_img(mdPath, assetPath='assets'):
-    with open(mdPath, 'r', encoding='utf-8') as file:
-        content = file.read()
-        print(content)
-        pattern = r'!\[.*\]\(assets/(.*?)\)'
-        matches = re.findall(pattern, content)
-        dealedMatches = []
-        for item in matches:
-            dealedMatches.append(decodeSpace(item))
-        print('md文件中出现的图片：', dealedMatches)
+# 该函数原本的mdPath是一个字符串，只会读取一个文件，适用于图片文件夹为【filename.assets】的模式，但不适用于【assets】的模式，因为后者可能会多个文件对应一个文件夹
+# 所以将其改为一个数组，读取传入的文件数组，并和文件路径进行对比。为了兼容filename.assets模式，该模式传入时将mdPath转化为一个元素的数组
+def delete_img(mdPaths, assetPath='assets'):
+    dealedMatchesList = []
+    for mdPath in mdPaths:
+        with open(mdPath, 'r', encoding='utf-8') as file:
+            content = file.read()
+            # print(content)
+            pattern = r'!\[.*\]\(.*assets[\\/](.*?)\)'
+            matches = re.findall(pattern, content)
+            dealedMatches = []
+            for item in matches:
+                dealedMatches.append(decodeSpace(item))
+            dealedMatchesList += dealedMatches
+    print('md文件中出现的图片：', dealedMatchesList)
 
-        # 读取需要删除的图片的文件列表
-        fileList = os.listdir(assetPath)
-        print('文件夹中存在的图片：', fileList)
-        for f in fileList:
-            if f not in dealedMatches:
-                print('删除多余文件 ', f)
-                removedFilePath = os.path.join(assetPath, f)
-                os.remove(removedFilePath)
-                # 已删除的文件加入统计列表
-                deletedFiles.append(removedFilePath)
-                deletedNum = deletedNum + 1
-        print('删除完成\n')
+    # 读取需要删除的图片的文件列表
+    fileList = os.listdir(assetPath)
+    print('文件夹中存在的图片：', fileList)
+    for f in fileList:
+        if f not in dealedMatchesList:
+            print('删除多余文件 ', f)
+            removedFilePath = os.path.join(assetPath, f)
+            os.remove(removedFilePath)
+            # 已删除的文件加入统计列表
+            deletedFiles.append(removedFilePath)
+            # deletedNum = deletedNum + 1
+    print('删除完成\n')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -70,7 +99,7 @@ if __name__ == '__main__':
     mode = ''
     while mode != 'a' and mode != 'b':
         mode = input('输入a或b选择不同的图片文件夹识别模式：(a)assets （b) filename.assets\n')
-    print('选择模式', mode)
+    print('已选择模式', mode, "，开始遍历文件夹\n")
     traverse_delete('', mode)
     print('共删除以下文件：', deletedFiles)
-    print('共删除文件数：', deletedNum)
+    # print('共删除文件数：', deletedNum)
